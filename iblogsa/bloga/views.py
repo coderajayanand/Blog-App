82f631cc34278f369ea8f3cac3e85d0b4a5055ac
coderajayanand <ajayanand524@gmail.com>
2022-10-17 00:49:53 -0700

    ABC

---
+++ iblogsa/bloga/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect
+from bloga.models import Post,Category
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all()[:11]
+    cats = Category.objects.all()
+
+    data = {
+        'posts': posts,
+        'cats': cats
+    }
+    return render(request, 'home.html', data)
+
+
+
+def post(request, url):
+    post=Post.objects.get(url=url)
+    cats=Category.objects.all()
+    return render(request,'posts.html',{'post':post, 'cats':cats})
+
+def category(request, url):
+    cat=Category.objects.get(url=url)
+    posts=Post.objects.filter(cat=cat)
+    return render(request,"category.html",{'cat':cat,'posts':posts})
+
+def about(request):
+    return render(request,'about.html')
