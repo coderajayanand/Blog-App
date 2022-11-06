3abfb2a226a343bc73f5a357e3e90034b8530757
coderajayanand <ajayanand524@gmail.com>
2022-11-06 13:36:23 -0800

    ABC

--- iblogsa/iblogsa/settings.py
+++ iblogsa/iblogsa/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -133,3 +136,6 @@ MATERIAL_ADMIN_SITE = {
     'MAIN_BG_COLOR':  '#000',  # Admin site main color, css color should be specified
 
 }
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
